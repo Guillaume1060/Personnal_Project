5398b4368fee2a9f5f56dfc008e372eaeb2d8df1
Guillaume <breyerguillaume@gmail.com>
2023-05-28 12:45:55 +0200

    Add fetchEmail

--- front-end/src/containers/footer/Footer.jsx
+++ front-end/src/containers/footer/Footer.jsx
@@ -1,9 +1,44 @@
 import ArrowUpIcon from '../../composants/listIcons/icons/Icons/ArrowUpIcon';
 import classes from './footer.module.scss'
+import { useState } from 'react';
 
 
 
 const Footer = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const response = await fetch('https://newsletter-app-10050-default-rtdb.europe-west1.firebasedatabase.app/emails.json', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email })
+        });
+  
+        if (response.ok) {
+          // Le serveur a accepté la requête
+          console.log('E-mail envoyé avec succès');
+          // Réinitialiser le formulaire
+          setEmail('');
+        } else {
+          console.error('Erreur lors de l\'envoi de l\'e-mail');
+        }
+      } catch (error) {
+        console.error('Erreur réseau', error);
+      }
+    };
+  
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+    };
+
+
+
+
     return (
         <div id='footer' className={classes.ctn}>
         <ArrowUpIcon/>
@@ -12,8 +47,8 @@ const Footer = () => {
                     <h2>SUSCRIBE</h2>
                     <p>Get updates, drops, advance ticket infomation and more direct to your inbox. No spam!</p>
                 </div>
-                <form method='post'>
-                    <input className={classes.footer_input} type={'email'} placeholder="GOT EMAIL?"/>
+                <form onSubmit={handleSubmit}>
+                    <input className={classes.footer_input} id="email" name="email" type={email} value={email} onChange={handleEmailChange} placeholder="GOT EMAIL?"/>
                 <button type='submit' name='subscribe'>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
